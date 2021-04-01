8f7c2e677b1c0a7b5cf3de981bfe55e474eb37c6
MrBaoquan <mrma617@gmail.com>
2021-04-01 14:10:40 +0800

    fixed debounce error.

--- Scripts/Core/TimerManager/UTimerManager.cs
+++ Scripts/Core/TimerManager/UTimerManager.cs
@@ -1,50 +1,46 @@
-﻿using System.Timers;
-using System;
+﻿using System;
 using System.Collections;
 using System.Collections.Generic;
-using UnityEngine;
-
-using UNIHper;
+using System.Timers;
 using UniRx;
+using UnityEngine;
+using UNIHper;
 
-public class UTimerManager : Singleton<UTimerManager>,Manageable
-{
+public class UTimerManager : Singleton<UTimerManager>, Manageable {
 
     Timer _timerDbc;
     Timer _timerTrt;
 
-    public void Initialize(){}
+    public void Initialize () { }
 
-    public void Uninitialize(){}
+    public void Uninitialize () { }
 
-    public IDisposable SetTimeout(Action InHandler, float InTime){
-        return SetTimeout(InTime,InHandler);
+    public IDisposable SetTimeout (Action InHandler, float InTime) {
+        return SetTimeout (InTime, InHandler);
     }
 
-    public IDisposable SetTimeout(float InDuration, Action OnCompleted = null, Action<float> OnUpdate = null, float InInterval=0.05f)
-    {
+    public IDisposable SetTimeout (float InDuration, Action OnCompleted = null, Action<float> OnUpdate = null, float InInterval = 0.05f) {
         float _startTime = Time.time;
         IDisposable _timerHandler = null;
-        _timerHandler = Observable.Interval(TimeSpan.FromSeconds(InInterval)).Where((_1,_2)=>{
+        _timerHandler = Observable.Interval (TimeSpan.FromSeconds (InInterval)).Where ((_1, _2) => {
             float _delta = Time.time - _startTime;
             bool _condition = _delta <= InDuration;
-            if(_condition){
-                float _progress = Mathf.Clamp(_delta / InDuration, 0 ,1);
-               if(OnUpdate!=null) OnUpdate(_progress);
-            }else{
-                if(OnUpdate!=null) OnUpdate(1.0f);
-                _timerHandler.Dispose();
-                if(OnCompleted!=null) OnCompleted();
+            if (_condition) {
+                float _progress = Mathf.Clamp (_delta / InDuration, 0, 1);
+                if (OnUpdate != null) OnUpdate (_progress);
+            } else {
+                if (OnUpdate != null) OnUpdate (1.0f);
+                _timerHandler.Dispose ();
+                if (OnCompleted != null) OnCompleted ();
             }
             return _condition;
-        }).Subscribe(_=>{});
+        }).Subscribe (_ => { });
         return _timerHandler;
     }
 
-    public IDisposable SetInterval(Action InCallback, float InInterval)
-    {
-        return Observable.Interval(TimeSpan.FromSeconds(InInterval)).Subscribe(_=>{
-            if(InCallback!=null) InCallback();
+    public IDisposable SetInterval (Action InCallback, float InInterval) {
+        return Observable.Interval (TimeSpan.FromSeconds (InInterval)).Subscribe (_ => {
+            if (InCallback != null) InCallback ();
         });
 
     }
@@ -55,49 +51,49 @@ public class UTimerManager : Singleton<UTimerManager>,Manageable
     /// <param name="InTime"></param>      
     /// <param name="InCallback"></param>   主动调用时: 计时器小于InTime时  调用将会被忽略  计时器大于InTime时，回调一次  计时器清零
     /// <returns></returns>
-    public Action Throttle(float InTime, Action InAction)
-    {
+    public Action Throttle (float InTime, Action InAction) {
         float _last = 0;
-        return ()=>{
+        return () => {
             float _delta = Time.time - _last;
-            if(_delta>=InTime){
-                InAction();
+            if (_delta >= InTime) {
+                InAction ();
                 _last = Time.time;
             }
         };
     }
-    
+
     /// <summary>
     /// 忽略InTime时间内的多次调用
     /// </summary>
     /// <param name="InTime"></param>       
     /// <param name="InCallback"></param>   主动调用后: 计时器清零, 计时器大于InTime时，回调一次, 计时器重新计时
     /// <returns></returns>
-    public Action Debounce(float InTime, Action InCallback)
-    {
+    public Action Debounce (float InTime, Action InCallback) {
         IDisposable _timerHandler = null;
-        float _lastCallTime =Time.time;
-        Func<long,bool> _condition = _=>{
-            return (Time.time - _lastCallTime)>=InTime;
+        float _lastCallTime = Time.time;
+        Func<long, bool> _condition = _ => {
+            return (Time.time - _lastCallTime) >= InTime;
         };
 
-        _timerHandler = Observable.EveryUpdate()
-                .Where(_condition)
-                .Subscribe(_=>{
-                    _lastCallTime = Time.time;
-                    InCallback();
-                });
-        return ()=>{
+        _timerHandler = Observable.EveryUpdate ()
+            .Where (_condition)
+            .Subscribe (_ => {
+                _lastCallTime = Time.time;
+                if (_timerHandler != null) {
+                    _timerHandler.Dispose ();
+                    _timerHandler = null;
+                }
+                InCallback ();
+            });
+        return () => {
             _lastCallTime = Time.time;
         };
     }
 
-    public void NextFrame(Action InAction){
-        Observable.NextFrame().Subscribe(_=>{
-            if(InAction!=null) InAction();
+    public void NextFrame (Action InAction) {
+        Observable.NextFrame ().Subscribe (_ => {
+            if (InAction != null) InAction ();
         });
     }
-
-
 
 }
