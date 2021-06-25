2f471f9894c47877bafa9c5ecff97d1f4adb98b8
MrBaoquan <mrma617@gmail.com>
2021-06-25 10:36:52 +0800

    分离http get&download.

--- Scripts/Foundation/HttpRequest.cs
+++ Scripts/Foundation/HttpRequest.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections;
+using System.IO;
 using System.Threading;
 using UniRx;
 using UnityEngine;
@@ -29,8 +30,34 @@ namespace UNIHper {
         /// <returns></returns>
         public static IObservable<string> Get (string url) {
             // convert coroutine to IObservable
-            return Observable.FromCoroutine<HttpResponse> ((observer, cancellationToken) => GetCore (url, false, observer, cancellationToken))
-                .Select (_response => _response.WebRequest.downloadHandler.text);
+            return Observable.FromCoroutine<string> ((observer, cancellationToken) => GetCore (url, observer, cancellationToken))
+                .Select (_response => _response);
+        }
+
+        static IEnumerator GetCore (string url, IObserver<string> observer, CancellationToken cancellationToken) {
+            using (UnityWebRequest webRequest = UnityWebRequest.Get (url)) {
+                // Request and wait for the desired page.
+                yield return webRequest.SendWebRequest ();
+
+                string[] pages = url.Split ('/');
+                int page = pages.Length - 1;
+
+                switch (webRequest.result) {
+                    case UnityWebRequest.Result.ConnectionError:
+                    case UnityWebRequest.Result.DataProcessingError:
+                        Debug.LogError (pages[page] + ": Error: " + webRequest.error);
+                        observer.OnError (new Exception (webRequest.error));
+                        break;
+                    case UnityWebRequest.Result.ProtocolError:
+                        Debug.LogError (pages[page] + ": HTTP Error: " + webRequest.error);
+                        observer.OnError (new Exception (webRequest.error));
+                        break;
+                    case UnityWebRequest.Result.Success:
+                        observer.OnNext (webRequest.downloadHandler.text);
+                        observer.OnCompleted ();
+                        break;
+                }
+            }
         }
 
         /// <summary>
@@ -38,15 +65,24 @@ namespace UNIHper {
         /// </summary>
         /// <param name="url"></param>
         /// <returns></returns>
-        public static IObservable<HttpResponse> Download (string url) {
-            return Observable.FromCoroutine<HttpResponse> ((observer, cancellationToken) => GetCore (url, true, observer, cancellationToken));
+        public static IObservable<HttpResponse> Download (string url, string savePath) {
+            return Observable.FromCoroutine<HttpResponse> ((observer, cancellationToken) => DownloadCore (url, savePath, true, observer, cancellationToken));
         }
 
         ///
         /// 注意: 在安卓平台有内存限制，不能下载大文件      TODO: 大文件支持
         // Note: IObserver's basic scheme is "OnNext* (OnError | Oncompleted)?" 
-        static IEnumerator GetCore (string url, bool updateSpeed, IObserver<HttpResponse> observer, CancellationToken cancellationToken) {
-            UnityWebRequest webRequest = UnityWebRequest.Get (url);
+        static IEnumerator DownloadCore (string url, string savePath, bool updateSpeed, IObserver<HttpResponse> observer, CancellationToken cancellationToken) {
+            UnityWebRequest webRequest = new UnityWebRequest (url);
+            webRequest.method = UnityWebRequest.kHttpVerbGET;
+            var _downloadHandler = new DownloadHandlerFile (savePath);
+            _downloadHandler.removeFileOnAbort = true;
+            webRequest.downloadHandler = _downloadHandler;
+
+            if (!Directory.Exists (Path.GetDirectoryName (savePath))) {
+                Directory.CreateDirectory (Path.GetDirectoryName (savePath));
+            }
+
             //yield return webRequest.SendWebRequest ();
             var _requestAction = webRequest.SendWebRequest ();
             // 开始下载时间
