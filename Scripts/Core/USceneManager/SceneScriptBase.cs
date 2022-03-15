c08a819e874a60d334085ca052909eb7bcafc2f8
MrBaoquan <mrma617@gmail.com>
2022-03-15 11:36:14 +0800

    add git-lfs && track *.so *.a *.dll

--- Scripts/Core/USceneManager/SceneScriptBase.cs
+++ Scripts/Core/USceneManager/SceneScriptBase.cs
@@ -3,31 +3,29 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-namespace UNIHper
-{
+namespace UNIHper {
 
-public abstract class SceneScriptBase
-{
-    /// <summary>
-    /// called once when the scene has been loaded
-    /// </summary>
-    public virtual void Start(){}
+    public abstract class SceneScriptBase {
+        /// <summary>
+        /// called once when the scene has been loaded
+        /// </summary>
+        public virtual void Start () { }
 
-    /// <summary>
-    /// called every frame after scene has been loaded
-    /// </summary>
-    public virtual void Update(){}
+        /// <summary>
+        /// called every frame after scene has been loaded
+        /// </summary>
+        public virtual void Update () { }
 
-    /// <summary>
-    /// called when the scene has been unloaded
-    /// </summary>
-    public virtual void OnDestroy(){}
-    
-    /// <summary>
-    /// called when application quit
-    /// </summary>
-    public virtual void OnApplicationQuit(){}
+        /// <summary>
+        /// called when the scene has been unloaded
+        /// </summary>
+        public virtual void OnDestroy () { }
 
-}
+        /// <summary>
+        /// called when application quit
+        /// </summary>
+        public virtual void OnApplicationQuit () { }
+
+    }
 
 }
